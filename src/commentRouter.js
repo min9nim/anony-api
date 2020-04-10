ce8956d83ba85ff6461b3f39fdae6cbab24a937b
mgsong <mgsong@jjsoft.kr>
2020-04-10 10:37:23 +0900

    use constant

--- src/commentRouter.js
+++ src/commentRouter.js
@@ -4,6 +4,8 @@ const Comment = require('./models/comment')
 const Post = require('./models/post')
 
 const R = require('ramda')
+const SUCCESS = 'Success'
+const FAIL = 'Fail'
 
 const router = express.Router()
 module.exports = router
@@ -19,7 +21,7 @@ function maskComment(comment) {
 }
 
 function sendErr(res) {
-  return err => {
+  return (err) => {
     console.log(err)
     res.status(500).send(err.toString())
   }
@@ -30,10 +32,10 @@ const get = {}
 
 // 부모글의 댓글카운트 세팅
 function setPostCommentCnt(postKey) {
-  return Post.findOne({ key: postKey }).then(post => {
-    Comment.find({ $and: [{ postKey: post.key }] }).then(comments => {
+  return Post.findOne({ key: postKey }).then((post) => {
+    Comment.find({ $and: [{ postKey: post.key }] }).then((comments) => {
       post.commentCnt = comments.length
-      post.save().then(output => {
+      post.save().then((output) => {
         console.log(output)
         console.log(`set post(${post.key})'s commentCnt : ${post.commentCnt}`)
       })
@@ -47,7 +49,7 @@ post['/add'] = (req, res) => {
 
   // post 상태가 삭제된 상태라면 댓글 등록 불가
   Post.findOne({ key: req.body.postKey })
-    .then(post => {
+    .then((post) => {
       if (post.deleted) {
         throw Error(`post(${req.body.postKey}) is deleted`)
       }
@@ -64,12 +66,12 @@ post['/add'] = (req, res) => {
 
       comment
         .save()
-        .then(output => {
+        .then((output) => {
           // 부모post 댓글카운트 set
           setPostCommentCnt(req.body.postKey)
 
           res.send({
-            status: 'success',
+            status: SUCCESS,
             message: `comment(${req.body.key}) is saved`,
             output,
           })
@@ -104,13 +106,13 @@ get['/get/:idx/:cnt'] = (req, res) => {
     .sort({ date: -1 })
     .skip(idx)
     .limit(cnt)
-    .then(comments => {
+    .then((comments) => {
       //console.log(JSON.stringify(comments, null,2));
       let res = R.map(maskComment)(comments)
       //console.log(JSON.stringify(res, null,2));
       return res
     })
-    .then(comments => res.send({ status: 'Success', comments: comments }))
+    .then((comments) => res.send({ status: SUCCESS, comments: comments }))
     .catch(sendErr(res))
 }
 
@@ -118,22 +120,22 @@ get['/get/:idx/:cnt'] = (req, res) => {
 get['/delete/:key/:uuid'] = (req, res) => {
   console.log(`/comments/delete/:key/:uuid call`)
   Comment.findOne({ key: req.params.key })
-    .then(comment => {
+    .then((comment) => {
       console.log('# comments = ' + JSON.stringify(comment, null, 2))
       if (comment.uuid === req.params.uuid) {
         comment.deleted = true
-        comment.save().then(output => {
+        comment.save().then((output) => {
           // 댓글 카운트 set
           setPostCommentCnt(comment.postKey)
 
           res.send({
-            status: 'Success',
+            status: SUCCESS,
             message: `comment(${req.params.key}) is deleted`,
             output,
           })
         })
       } else {
-        res.send({ status: 'Fail', message: 'Not authorized' })
+        res.send({ status: FAIL, message: 'Not authorized' })
       }
     })
     .catch(sendErr(res))
@@ -143,21 +145,21 @@ get['/delete/:key/:uuid'] = (req, res) => {
 get['/remove/:key/:uuid'] = (req, res) => {
   console.log(`/comments/remove/:key/:uuid call`)
   Comment.findOne({ key: req.params.key })
-    .then(comment => {
+    .then((comment) => {
       console.log('# comments = ' + JSON.stringify(comment, null, 2))
       if (comment.uuid === req.params.uuid) {
-        Comment.remove({ key: req.params.key }).then(output => {
+        Comment.remove({ key: req.params.key }).then((output) => {
           // 댓글 카운트 set
           setPostCommentCnt(comment.postKey)
 
           res.send({
-            status: 'Success',
+            status: SUCCESS,
             message: `comment(${req.params.key}) is deleted`,
             output,
           })
         })
       } else {
-        res.send({ status: 'Fail', message: 'Not authorized' })
+        res.send({ status: FAIL, message: 'Not authorized' })
       }
     })
     .catch(sendErr(res))
@@ -166,28 +168,28 @@ get['/remove/:key/:uuid'] = (req, res) => {
 // key 에 해당하는 포스트의 comment 를 조회
 get['/get/:key'] = (req, res) => {
   Comment.find({ postKey: req.params.key })
-    .then(comment => {
+    .then((comment) => {
       console.log(comment)
       return comment
     })
     .then(R.map(maskComment))
-    .then(comments => res.send({ status: 'Success', comments: comments }))
+    .then((comments) => res.send({ status: SUCCESS, comments: comments }))
     .catch(sendErr(res))
 }
 
 // key에 해당하는 코멘트의 작성자가 맞는지 확인
 get['/auth/:key/:uuid'] = (req, res) => {
   Comment.findOne({ key: req.params.key })
-    .then(comment => {
+    .then((comment) => {
       console.log(comment)
       if (comment.uuid === req.params.uuid) {
         res.send({
-          status: 'Success',
+          status: SUCCESS,
           message: 'Authorized successfully',
           comment: maskComment(comment),
         })
       } else {
-        res.send({ status: 'Fail', message: 'Not authorized' })
+        res.send({ status: FAIL, message: 'Not authorized' })
       }
     })
     .catch(sendErr(res))
@@ -197,12 +199,12 @@ get['/auth/:key/:uuid'] = (req, res) => {
 post['/edit/:uuid'] = (req, res) => {
   //console.log("received data = " + JSON.stringify(req.body, null, 2));
   Comment.findOne({ key: req.body.key })
-    .then(comment => {
+    .then((comment) => {
       //console.log("#### 검색결과");
       //console.log(JSON.stringify(comment, null, 2));
 
       if (comment.uuid !== req.params.uuid) {
-        res.send({ status: 'Fail', message: 'Not authorized' })
+        res.send({ status: FAIL, message: 'Not authorized' })
         return
       }
 
@@ -213,9 +215,9 @@ post['/edit/:uuid'] = (req, res) => {
       //console.log(JSON.stringify(comment, null, 2));
       comment
         .save()
-        .then(output => {
+        .then((output) => {
           res.send({
-            status: 'Success',
+            status: SUCCESS,
             message: `comment@${req.body.key} updated.`,
             output: maskComment(output),
           })
@@ -228,19 +230,19 @@ post['/edit/:uuid'] = (req, res) => {
 // delete 된 댓글을 복원
 get['/restore/:key/:uuid'] = (req, res) => {
   Comment.findOne({ key: req.params.key })
-    .then(comment => {
+    .then((comment) => {
       if (comment.uuid === req.params.uuid) {
         comment.deleted = false
-        comment.save().then(output => {
+        comment.save().then((output) => {
           console.log(output)
           res.send({
-            status: 'Success',
+            status: SUCCESS,
             message: `comment(${req.params.key}) is restored`,
             output: maskComment(output),
           })
         })
       } else {
-        res.send({ status: 'Fail', message: 'Not authorized' })
+        res.send({ status: FAIL, message: 'Not authorized' })
       }
     })
     .catch(sendErr(res))
