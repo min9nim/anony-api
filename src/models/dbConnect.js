d8c862576c4e64831defd2a700a5aa0f6517c4d5
Keating <min9nim@gmail.com>
2023-04-14 06:02:38 +0900

    log dburl

--- src/models/dbConnect.js
+++ src/models/dbConnect.js
@@ -14,7 +14,7 @@ const dburl = process.env.dburl
 console.log('dburl:', dburl)
 mongoose
   .connect(dburl, { useNewUrlParser: true })
-  .then(success => console.log('DB 연결 성공'))
+  .then(success => console.log('DB 연결 성공', dburl))
   .catch(error => {
     console.warn('디비 연결 실패..')
     console.log('dburl:', dburl)
